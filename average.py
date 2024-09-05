5d4249c1821ac5795f060f7dbb1324465608d60d
Cameron <magicat8bill@gmail.com>
2024-09-05 14:32:04 -0600

    added functionality

--- average.py
+++ average.py
@@ -1,12 +1,15 @@
-"""
-Import the text_utils module you created and calculate the average number of
-words per line in a given text file. The text file that will be used to test
-this is the text file called "sample.txt" (located in the same directory as
-this exercise). The average number of words per line should be rounded down to
-the nearest integer.
+import text_utils
 
-Print the average number of words per line in the text file in the following
-format:
+# Open the file in read mode
+with open('sample.txt', 'r') as file:
+    # Read all lines from the file
+    lines = file.readlines()
 
-"Average words per line: [average]"
-"""
+# Count the total number of words in the file
+total_words = text_utils.count_words(''.join(lines))
+
+# Calculate the average number of words per line
+average_words_per_line = total_words // len(lines)
+
+# Print the result
+print(f"Average words per line: {average_words_per_line}")
--- text_utils.py
+++ text_utils.py
@@ -1,14 +1,11 @@
+# Counts charicters in string
 def count_chars(text):
-    """
-    Count the number of characters in a text
-    """
+    return len(text)
 
+# Counts words in string
 def count_words(text):
-    """
-    Count the number of words in a text
-    """
+    return len(text.split())
 
+# Counts sentences in string
 def count_sentences(text):
-    """
-    Count the number of sentences in a text
-    """
+    return text.count(".") + text.count("!") + text.count("?")
